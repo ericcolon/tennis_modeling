63588ce5a48177b2a34ee44a859da9b937622418
Sidd Jagadish <sidd@affirm.com>
2018-05-20 02:24:10 -0700

    Added logistic regression with non-zero prior

---
+++ ml/prior_logit.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
+
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+
+class NonZeroLogit():
+
+    def __init__(self, lmbda=0.1, prior=0.):
+        self.lmbda = lmbda
+        self.prior = prior
+
+    @staticmethod
+    def loss(beta, X, y, lmbda, prior, w):
+        # Negate likelihood because we lbfgs minimizes
+        y_hat = sigmoid(X.dot(beta))
+        return -np.sum(w *
+            (y * np.log(y_hat) + (1. - y) * np.log(1. - y_hat))
+        ) + lmbda * (beta - prior).transpose().dot(beta - prior)
+
+    @staticmethod
+    def fprime(beta, X, y, lmbda, prior, w):
+        y_hat = sigmoid(X.dot(beta))
+        return -(X.transpose().dot(w * (y - y_hat))) + lmbda * (beta - prior)
+
+    def fit(self, X, y, sample_weight=None):
+        self.beta = np.random.normal(size=X.shape[1])  # Initialize randomly
+        if sample_weight is None:
+            w = np.ones(X.shape[0])
+        else:
+            w = sample_weight
+        result = fmin_l_bfgs_b(
+            self.loss,
+            x0 = self.beta,
+            args = (X, y, self.lmbda, self.prior, w),
+            fprime = self.fprime,
+            pgtol =  1e-3,
+            disp = True
+        )
+        self.beta = result[0]
+
+    def predict_proba(self, X):
+        p1 = sigmoid(X.dot(self.beta))
+        return np.array([1. - p1, p1]).transpose()
+
+    def predict(self, X):
+        return (sigmoid(X.dot(self.beta)) > 0.5).astype(int)
