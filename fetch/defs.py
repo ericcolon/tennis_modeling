fa8fdad3bd5e175b7bf15742998130616dc83965
Sidd Jagadish <sidd@affirm.com>
2018-11-03 18:14:55 -0700

    Added fetch defs

---
+++ fetch/defs.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+ROUND_MAP = {
+    '1st Round': 1,
+    '2nd Round': 2,
+    '3rd Round': 3,
+    '4th Round': 4,
+    'Quarterfinals': 5,
+    'Semifinals': 6,
+    'The Final': 7,
+    'Round Robin': np.nan
+}
